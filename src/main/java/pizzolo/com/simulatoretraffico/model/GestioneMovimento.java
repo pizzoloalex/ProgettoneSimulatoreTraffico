e19ebd4ab7008107b2af3e3917bf0d6ac9ddae61
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-04-14 22:21:39 +0200

    inizializzato movimento

--- src/main/java/pizzolo/com/simulatoretraffico/model/GestioneMovimento.java
+++ src/main/java/pizzolo/com/simulatoretraffico/model/GestioneMovimento.java
@@ -1,21 +1,27 @@
 package pizzolo.com.simulatoretraffico.model;
 
 import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
+
 import java.util.ArrayList;
 
 public class GestioneMovimento extends AnimationTimer{
-    private Macchina macchina;
     //gestisce l'array di macchine per tutto il movimento di ogni macchina
     private ArrayList<Macchina> macchineCanvas;
     //gestisce il movimento della macchine
-    private AnimationTimer animationTimer;
-    public GestioneMovimento(){
+    private GraphicsContext gc;
+    public GestioneMovimento(GraphicsContext gc){
         macchineCanvas = new ArrayList<>();
+        this.gc = gc;
     }
 
     @Override
     public void handle(long l) {
-
+        gc.clearRect(0,0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+        for (Macchina m: macchineCanvas){
+            m.aggiorna(gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+            m.disegna(gc);
+        }
     }
 
     public ArrayList<Macchina> getMacchineCanvas() {
