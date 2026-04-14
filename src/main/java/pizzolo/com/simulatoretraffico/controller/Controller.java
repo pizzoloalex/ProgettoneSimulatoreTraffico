0af3afe0cbad39020bf44e30343de4dfdd9a7664
pzale <alex.pizzolo@edu.iisleviponti.it>
2026-04-14 18:59:19 +0200

    aggiunto metodo che elimina l'ultima macchina disegnata

--- src/main/java/pizzolo/com/simulatoretraffico/controller/Controller.java
+++ src/main/java/pizzolo/com/simulatoretraffico/controller/Controller.java
@@ -6,6 +6,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
 import pizzolo.com.simulatoretraffico.model.Macchina;
 
+import java.util.ArrayList;
+
 public class Controller {
     @FXML
     private Canvas canvas;
@@ -16,14 +18,39 @@ public class Controller {
     private Macchina macchina;
     //tiene il conto di quante macchine ci sono
     private int numMacchina;
+    private ArrayList<Macchina> macchineCanvas;
 
     public void initialize() {
         gc = canvas.getGraphicsContext2D();
+        macchineCanvas = new ArrayList<>();
     }
 
+
+    /**
+     * metodo che disegna una macchina e salva in un array
+     */
+    @FXML
     public void aggiungiMacchina() {
         macchina = new Macchina(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        macchineCanvas.add(macchina);
         macchina.disegna(gc);
+        //salva la macchina appena disegnata
         numMacchina++;
+    }
+
+    /**
+     * metodo che elimina l'ultima macchina aggiunta ridisegnando il canvas
+     */
+    @FXML
+    public void eliminaMacchina() {
+        if (macchineCanvas.isEmpty()) {
+            return;
+        }
+        macchineCanvas.removeLast();
+        numMacchina--;
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for (Macchina m : macchineCanvas) {
+            m.disegna(gc);
+        }
     }
 }
